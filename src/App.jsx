821bd3a5c478a3b034c547a0fc610c6a86e1a6a4
Gyenes Gergo <gergo.gyenes@gmail.com>
2024-01-10 12:08:44 +0100

    Making the structure and sections

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,40 @@
+import {
+    Hero,
+    PopularProducts,
+    SuperQuality,
+    Services,
+    SpecialOffer,
+    CustomerReviews,
+    Subscribe,
+    Footer,
+} from "./sections";
+
 const App = () => (
     <main className="relative">
-        Nav
+        {/*<Nav />*/}
         <section className="xl:padding-1 wide:padding:r padding-b">
-            Hero
+            <Hero />
         </section>
-        <section className="padding">PopularProducts</section>
-        <section className="padding">SuperQuality</section>
-        <section className="padding-x py-10">Services</section>
-        <section className="padding">SpecialOffer</section>
-        <section className="padding bg-pale-blue">CustomerReviews</section>
-        <section className="padding-x sm:py-32 py-16 w-full">Subscribe</section>
+        <section className="padding">
+            <PopularProducts />
+        </section>
+        <section className="padding">
+            <SuperQuality />
+        </section>
+        <section className="padding-x py-10">
+            <Services />
+        </section>
+        <section className="padding">
+            <SpecialOffer />
+        </section>
+        <section className="padding bg-pale-blue">
+            <CustomerReviews />
+        </section>
+        <section className="padding-x sm:py-32 py-16 w-full">
+            <Subscribe />
+        </section>
         <section className="padding bg-black padding-x padding-t pb-8">
-            Footer
+            <Footer />
         </section>
     </main>
 );
