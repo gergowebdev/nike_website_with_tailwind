41ba7aa26a24de81b1893a20b6e196bcefe12dd8
Gyenes Gergo <gergo.gyenes@gmail.com>
2024-01-11 11:16:58 +0100

    Services and Special offer sections

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,6 +1,19 @@
-const Button = ({ label, iconUrl }) => {
+const Button = ({
+    label,
+    iconUrl,
+    backgroundColor,
+    borderColor,
+    textColor,
+}) => {
     return (
-        <button className="flex items-center justify-center gap-2 px-7 py-4 border font-montserrat text-lg leading-none bg-coral-red rounded-full text-white border-coral-red">
+        <button
+            className={`flex items-center justify-center gap-2 px-7 py-4 border font-montserrat text-lg leading-none
+            ${
+                backgroundColor
+                    ? `${backgroundColor} ${textColor} ${borderColor}`
+                    : " bg-coral-red text-white border-coral-red"
+            } rounded-full`}
+        >
             {label}{" "}
             {iconUrl && (
                 <img
